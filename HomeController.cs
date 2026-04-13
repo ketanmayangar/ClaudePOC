676a01b69d4d722c52b25b6454095f7320ff2e4a
Ketan Mayangar <63995942+ketanmayangar@users.noreply.github.com>
2026-04-13 15:52:17 +0530

    Add files via upload

--- HomeController.cs
+++ HomeController.cs
@@ -28,5 +28,10 @@ namespace WebApplication1.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult NonPrivacy()
+        {
+            return View();
+        }
     }
 }
